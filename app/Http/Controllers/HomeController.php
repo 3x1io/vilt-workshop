ede10398e70c997f0537aad93425c8ae30923108
Fady Mondy <EngFadyMondy@gmail.com>
2023-07-03 23:48:23 +0300

    first commit 🔥

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
+    //return a welcome view
     public function index()
     {
         return view('welcome');
+    }
+
+    //return about view
+    public function about()
+    {
+        return view('about');
     }
 }
---
+++ resources/js/Components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <ul v-if="">
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        data() {
+            return {
+                //
+            }
+        },
+        methods: {
+            //
+        },
+        mounted() {
+            //
+        }
+    }
+</script>
